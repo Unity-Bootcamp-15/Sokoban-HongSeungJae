82f06ceb3977a6f5c4787c6bb8ce20b81f92e25c
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-11-28 10:25:20 +0900

    feat: 초기 프로젝트 추가

---
+++ Sokoban-HongSeungJae/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sokoban_HongSeungJae
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
