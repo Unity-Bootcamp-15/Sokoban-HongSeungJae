8214f7f98f8d0447997b2177d1236f7283d3f830
tfarcenim123-afk <tfarcenim123@gmail.com>
2025-11-28 11:06:03 +0900

    feat: 셋커서포지션 써보기

--- Sokoban-HongSeungJae/Program.cs
+++ Sokoban-HongSeungJae/Program.cs
@@ -6,11 +6,18 @@
         {
 
                 Console.ResetColor();
-                Console.CursorVisible = false;
+                //Console.CursorVisible = false;
                 Console.Title = "SOKOBAN SEUNGJAE";
                 Console.BackgroundColor = ConsoleColor.Magenta;
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Clear();
+                
+            for (int i = 0; i < 101; i++)
+            {
+                Console.SetCursorPosition(i, i);
+                Console.Write("o");
+            }
+                
 
         }
     }
